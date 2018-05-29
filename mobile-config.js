5fccba258ca38795e21eb1433f8c15e34ff5282b
Mesiboku <konamispica@gmail.com>
2018-05-29 09:31:02 -0700

    Initial Mobile Configuration for Mobile

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+    id: 'com.arjel.chat',
+    name: 'Messanger',
+    description: 'Messanger at its gwapo',
+    author: 'Arjel Angelo Ramos',
+    email: 'arjelangelo.ramos@gmail.com',
+    website: 'http://arjelramos.com'
+});
+
+
+/*
+meteor build ~/home/mesiboku/Meteor-Build --server=http://192.168.1.100:3000 --verbose
+*/
